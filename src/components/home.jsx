b1c97ba821ca1ecbf513de7eb92e20e96e1ebc1a
josh <joshwambere@gmail.com>
2020-09-11 10:36:27 +0200

    heroku API and mongo db connection

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -5,6 +5,7 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import loader from './img/loader.gif';
+import axios from 'axios';
 
 library.add(faTrash)
 
@@ -17,37 +18,57 @@ class Home extends Component {
         isLoading: false
     };
     addTodo = (e) => {
-        this.setState({isLoading:true})
-        this.setState({ todo: e });
-        let newArr = this.state.todos;
-        newArr.push(e)
-        this.setState({ todos: newArr });
-        this.setState({isLoading:false})
+        this.setState({ isLoading: true })
+        this.loading = true
+        axios.post('https://joh-n-son-react-api.herokuapp.com/api/todos/add', {
+            name: e.title,
+            completed: e.completed
+        })
+
+            .then(res => this.setState({ todos: [...this.todos, res.data] }))
+            .catch(err => console.log(err))
+            .finally(() => (this.setState({ isLoading: false })))
+
     }
+
+
     popOutTodo = (id) => {
-        this.setState({isLoading:true})
+        this.setState({ isLoading: true })
         let Items = this.state.todos;
-        let i = Items.findIndex(popoutItem => popoutItem === id);
-        Items.splice(i, 1)
-        this.setState({ todos: Items });
-        this.setState({isLoading:false})
+        axios.delete(`https://joh-n-son-react-api.herokuapp.com/api/todo/delete/${id}`)
+            .then(this.Items = this.setState({ todos: Items.filter(todo => todo._id !== id) })
+            )
+            .catch(err => console.log(err))
+            .finally(() => (this.setState({ isLoading: false })))
+
 
     }
     todoCompleted(id) {
-        this.setState({isLoading:true})
+        this.setState({ isLoading: true })
         const Items = this.state.todos.map(todo => {
-            if (todo.id === id) {
+            if (todo._id === id) {
                 todo.completed = !todo.completed;
             }
             return todo;
         });
         this.setState({ todos: Items });
-        this.setState({isLoading:false})
+        this.setState({ isLoading: false })
+    }
+    created = () => {
+
+        axios.get('https://joh-n-son-react-api.herokuapp.com/api/todos')
+            .then(res => this.setState({ todos: res.data }))
+            .catch(err => console.log(err));
+
+    }
+    componentDidMount() {
+        this.created();
     }
     render() {
 
         return (
             <div className="home-sect">
+                {this.created()}
                 <AddTodo newTodo={this.addTodo} />
                 <div className="addTodo pt-2">
                     <div className="container pt-0">
@@ -55,12 +76,12 @@ class Home extends Component {
                     </div>
                     <div className="container pt-1">
                         <span className="badge badge-info">Tasks</span>
-                        {this.state.todos.length===0? <h3 className="pt-2">No Task Yet</h3>:''}
+                        {this.state.todos.length === 0 ? <h3 className="pt-2">No Task Yet</h3> : ''}
                         {this.state.todos.map(item =>
-                            <div className="todo" key={item.id} >
-                                <input onClick={() => this.todoCompleted(item.id)} className="todo-input" type="checkbox" name="todo" />
-                                <label className="tod-lable" htmlFor="todo">{item.title}</label>
-                                <FontAwesomeIcon className="icon-trash" icon={["fas", "trash"]} onClick={() => this.popOutTodo(item.id)} />
+                            <div className="todo" key={item._id} >
+                                <input onClick={() => this.todoCompleted(item._id)} className="todo-input" type="checkbox" name="todo" />
+                                <label className="tod-lable" htmlFor="todo">{item.name}</label>
+                                <FontAwesomeIcon className="icon-trash" icon={["fas", "trash"]} onClick={() => this.popOutTodo(item._id)} />
                             </div>
                         )}
                     </div>
